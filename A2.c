41211f664df2eb2cd6f9c678c8071b434e906eac
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2022-12-26 21:50:51 +0530

    Add files via upload

---
+++ A2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int hour ,minutes ,seconds ,total_seconds;
+    printf("enter hour,minutes and seconds");
+    scanf("%d%d%d",&hour,&minutes,&seconds);
+    total_seconds= (3600*hour+60*minutes+seconds);
+    printf("your total seconds in %d hours , %d minutes and %d seconds is %d seconds",hour,minutes,seconds,total_seconds);
+    return 0;
+}
