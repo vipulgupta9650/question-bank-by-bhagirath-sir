1a39c535e8972bf5172c2a35f96f8a2bf83053af
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2022-12-27 23:07:24 +0530

    Add files via upload

---
+++ B5.c
@@ -0,0 +1,21 @@
+//c program to check armstrong number
+#include <stdio.h>
+int main()
+{
+int n,digits;
+printf("print number of digits in the number");
+scanf("%d",digits);
+printf("enter the number");
+scanf("%*d",digits,&n);
+fflush(stdin);
+int last_digit=n%10;
+while()
+{
+
+
+
+
+
+}
+    return 0;
+}
