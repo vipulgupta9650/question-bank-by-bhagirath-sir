65f3f5948ae057ff884410d4959c277375e6104a
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2022-12-27 22:01:19 +0530

    Add files via upload

---
+++ B0.c
@@ -0,0 +1,17 @@
+//c program to find factorial of a number n
+#include <stdio.h>
+int main()
+{
+int n;
+printf("enter n");
+scanf("%d",&n);
+int i=1;
+int factorial =1;
+while(i<=n)
+{factorial=factorial*i;
+  i++;
+    
+}
+printf("%d",factorial);
+    return 0;
+}
