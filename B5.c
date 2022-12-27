277021d55b0e63d8d590df7413794989c7c1f3a3
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2022-12-27 23:53:37 +0530

    Update B5.c

--- B5.c
+++ B5.c
@@ -1,21 +1,28 @@
 //c program to check armstrong number
 #include <stdio.h>
+#include <math.h>
 int main()
 {
 int n,digits;
-printf("print number of digits in the number");
-scanf("%d",digits);
-printf("enter the number");
-scanf("%*d",digits,&n);
+printf("print number of digits in the number and the number");
+scanf("%d%*d",&digits,digits,&n);
 fflush(stdin);
-int last_digit=n%10;
-while()
+int number=n;
+int sum=0;
+while(digits<=n)
 {
-
-
-
-
-
+    number=number % (int)pow(10,digits);
+    printf("%d\t",number);
+    sum=sum+pow(number,digits);
+      digits++;
+}
+if(sum=n)
+{
+    printf("number is an armstrong number");
+}
+else 
+{
+        printf("number is not an armstrong number");
 }
     return 0;
 }
