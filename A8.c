4b4137060395cf033c2ed08ca25e535fbef4c093
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2022-12-28 13:49:29 +0530

    Update A8.c

--- A8.c
+++ A8.c
@@ -1,17 +1,15 @@
-// swap two integer without using third variable
-#include <stdio.H>
+//swap two integer without using third variable
+#include <stdio.h>
 int main()
 {
 int a,b;
 printf("enter a and b");
 scanf("%d%d",&a,&b);
-a=b;
-printf("your new value of a is %d\n\n\n\n\n",a);
-printf("enter the initial value of a");
-scanf("%d",&a);
-b=a;
-printf("\n\n\nyour new value of b is %d",b);
-
+printf("your values of a and b are %d\t%d",a,b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("your new values of a and b are %d\t%d",a,b);
 
 
     return 0;
