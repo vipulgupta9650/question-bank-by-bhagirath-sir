78f23ac23ee71dbcca86d9202ecc54d739055fd3
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2022-12-28 18:53:38 +0530

    Add files via upload

---
+++ C5.c
@@ -0,0 +1,13 @@
+//c program to print the ASCII value of a character
+#include <stdio.h>
+int main()
+{
+char ch;
+printf("enter a character");
+ch=getchar();
+
+printf("%d",ch);
+
+
+    return 0;
+}
