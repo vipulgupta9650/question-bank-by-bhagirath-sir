732ee533fc19c17566f41d9e340f671f2ab46a77
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2022-12-29 22:45:41 +0530

    Add files via upload

---
+++ D1.c
@@ -0,0 +1,20 @@
+// write a program to print the factorial of a given number
+#include <stdio.h>
+int main()
+{
+int n,i;
+printf("enter the value of n");
+scanf("%d",&n);
+fflush(stdin);
+int factorial =1;
+for(i=1;i<=n;i++)
+{
+    factorial = factorial *i;
+}
+printf("\tyour factorial of the given number is %d",factorial);
+
+
+
+
+ return 0;
+}
