388846592bb075ab410ae959d2f5f982614167c5
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2023-01-05 22:54:42 +0530

    Update D2.c

--- D2.c
+++ D2.c
@@ -1,1 +1,27 @@
-// write a program to check if the entered number is prime or not
+//write a program to check whether the number is prime or not
+#include <stdio.h>
+int main()
+{
+int n;
+printf("enter the value of n");
+scanf("%d",&n);
+int flag =1;
+int i;
+for(i=2;i<=n/2;i++)
+{
+ if(n%i==0)
+ {
+    flag =0;
+    break;
+ }
+}
+if (flag ==1)
+{
+    printf(" prime");
+}
+else if (flag ==0)
+{
+    printf(" not prime");
+}
+ return 0;
+}
